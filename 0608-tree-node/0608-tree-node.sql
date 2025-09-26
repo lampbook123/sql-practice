f021f87f45a4365abb658f0d6778d191bab74577
lampbook123 <113386302+lampbook123@users.noreply.github.com>
2025-09-26 17:50:30 +0530

    Time: 419 ms (91.37%), Space: 0B (100%) - LeetHub

---
+++ 0608-tree-node/0608-tree-node.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement be
+select id, 
+case
+   when p_id is null then 'Root'
+   when id  not in (select distinct p_id from Tree where p_id is not null )
+   then 'Leaf'
+else 'Inner'
+  end as type
+from Tree;
